bc65ccba7c7e3fc28a5dd53e6a569d0c4b3d23b9
Carl Zhang <zhang.carl.x@gmail.com>
2019-07-28 16:58:22 -0400

    Initialize OpenGL and GLEW
    - Requested OpenGL attributes
    - Initialized GLEW in game class

--- Gold Rush/Game.cpp
+++ Gold Rush/Game.cpp
@@ -11,8 +11,7 @@ Game::~Game()
 {
 
 }
-// TODO: OpenGL set attributes (pg 129),
-// Render frames in GenerateOutput()
+// TODO: Render frames in GenerateOutput()
 int Game::Init()
 {
 	// Initialize SDL
@@ -21,7 +20,24 @@ int Game::Init()
 		std::cout << "SDL unable to initialize. SDL_Error: " << SDL_GetError() << std::endl;
 		return 1;
 	}
-	// Create the window
+
+	// Request OpenGL attributes
+	// Use core OpenGL profile
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+	// Version 3.3
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+	// 8-bit color buffer
+	SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
+	// Double buffering
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+	// Hardware acceleration
+	SDL_GL_SetAttribute(SDL_GL_ACCELERATED_VISUAL, 1);
+
+	// Create SDL window
 	mWindow = SDL_CreateWindow(
 		"Gold Rush",
 		SDL_WINDOWPOS_CENTERED,
@@ -35,6 +51,7 @@ int Game::Init()
 		std::cout << "Unable to create window: "  << SDL_GetError() << std::endl;
 		return 1;
 	}
+
 	// Create OpenGL context and initialize GLEW
 	mContext = SDL_GL_CreateContext(mWindow);
 	glewExperimental = true;
